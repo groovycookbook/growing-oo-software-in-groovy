2a7d0e2ee780f9f02b4e9b7b7efb92a282ec1737
Paul King <paulk@asert.com.au>
2010-01-22 10:53:47 +1000

    tweak structure

--- junit-windowlicker-tests/src/test/java/integration/auctionsniper/ui/MainWindowTest.java
+++ junit-windowlicker-tests/src/test/java/integration/auctionsniper/ui/MainWindowTest.java
@@ -13,22 +13,22 @@ import auctionsniper.ui.MainWindow;
 import com.objogate.wl.swing.probe.ValueMatcherProbe;
 
 public class MainWindowTest {
-  private final MainWindow mainWindow = new MainWindow(new SniperPortfolio());
-  private final AuctionSniperDriver driver = new AuctionSniperDriver(100);
+    private final MainWindow mainWindow = new MainWindow(new SniperPortfolio());
+    private final AuctionSniperDriver driver = new AuctionSniperDriver(100);
 
-  @Test public void
-  makesUserRequestWhenJoinButtonClicked() {
-    final ValueMatcherProbe<Item> itemProbe =
-      new ValueMatcherProbe<Item>(equalTo(new Item("an item-id", 789)),
-                                  "item request");
-    mainWindow.addUserRequestListener(
-        new UserRequestListener() {
-          public void joinAuction(Item item) {
-            itemProbe.setReceivedValue(item);
-          }
-        });
+    @Test
+    public void
+    makesUserRequestWhenJoinButtonClicked() {
+        final ValueMatcherProbe<Item> itemProbe =
+                new ValueMatcherProbe<Item>(equalTo(new Item("an item-id", 789)), "item request");
+        mainWindow.addUserRequestListener(
+                new UserRequestListener() {
+                    public void joinAuction(Item item) {
+                        itemProbe.setReceivedValue(item);
+                    }
+                });
 
-    driver.startBiddingWithStopPrice("an item-id", 789);
-    driver.check(itemProbe);
-  }
+        driver.startBiddingWithStopPrice("an item-id", 789);
+        driver.check(itemProbe);
+    }
 }
