c69ae29592a14c089400bd33daaadacb3181af7b
Paul King <paulk@asert.com.au>
2010-02-01 17:14:39 +1000

    finish inbuilt mocking examples

--- groovy-inbuilt-mocking-tests/src/test/groovy/unit/auctionsniper/xmpp/LoggingXMPPFailureReporterTest.groovy
+++ groovy-inbuilt-mocking-tests/src/test/groovy/unit/auctionsniper/xmpp/LoggingXMPPFailureReporterTest.groovy
@@ -10,9 +10,10 @@ import auctionsniper.xmpp.LoggingXMPPFailureReporter
 class LoggingXMPPFailureReporterTest extends GroovyTestCase {
 
     void testWritesMessageTranslationFailureToLog() {
-        Object[] args = ['foo', null]
-        def overrides = [severe:{ assert it == '<auction id> Could not translate message "bad message" because "java.lang.Exception: an exception"' }]
-        Logger mockLogger = ProxyGenerator.INSTANCE.instantiateAggregateFromBaseClass(overrides, Logger, args)
+        def overrides = [severe:{ msg ->
+            assert msg == '<auction id> Could not translate message "bad message" because "java.lang.Exception: an exception"' }]
+        Object[] loggerConstructorArgs = ['dummyName', null /* no resource bundle required */]
+        Logger mockLogger = ProxyGenerator.INSTANCE.instantiateAggregateFromBaseClass(overrides, Logger, loggerConstructorArgs)
         def reporter = new LoggingXMPPFailureReporter(mockLogger)
         reporter.cannotTranslateMessage("auction id", "bad message", new Exception("an exception"))
     }
